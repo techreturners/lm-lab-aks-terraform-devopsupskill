da370471c45e6114d449e3e96f69f7cd005efd86
James Heggs <james.heggs@techreturners.com>
2021-03-11 21:35:51 +0000

    Utilise system assigned identity to ease attaching of ACR

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -24,9 +24,9 @@ resource "azurerm_kubernetes_cluster" "default" {
     os_disk_size_gb = 30
   }
 
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
+  # azure will assign the id automatically
+  identity {
+    type = "SystemAssigned"
   }
 
   role_based_access_control {
